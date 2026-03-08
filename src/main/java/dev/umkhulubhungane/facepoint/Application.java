f83ff84fcec94fb7fdde737512556d07fb2ccbec
radebeneo <radebeneo17@gmail.com>
2026-03-08 22:15:17 +0200

    updated dependecies

--- src/main/java/dev/umkhulubhungane/facepoint/Application.java
+++ src/main/java/dev/umkhulubhungane/facepoint/Application.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FacepointApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FacepointApplication.class, args);
+
+		SpringApplication.run(Application.class, args);
 	}
 
 }
